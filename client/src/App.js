002fc1450f52669811e6745bc7ff35d8fc768596
jbxamora <jbxamora@icloud.com>
2023-03-30 23:27:01 -0700

    updated client files, added base styles,replaced logo, loaded test app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#2e4673] text-emerald-700">
+        Test
+        </header>
+    </BrowserRouter>
   );
 }
 
