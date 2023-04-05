75bdf447c961611c257caf881f2cc8881e7477e5
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-04-04 20:56:04 -0700

    added schemas and need to edit

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,87 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+   type User {
+    _id: ID!
+    username: String!
+    email: String!
+    recipeCount: Int
+    selectedRecipeIds:[ID]
+  }
+  type Recipe {
+   _id: ID!
+   name: String
+   image: String
+   createdAt: String
+   ingredients: String
+   steps: [String]
+   totalTime: Int
+   serves: Int
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  } 
+
+  type Comment {
+    id: ID!
+    body: String!
+    username: String!
+    createdAt: String!
+  }
+  type Reaction {
+    id: ID!
+    username: String!
+    createdAt: String!
+  }
+  type Query {
+    me: User
+    getRecipes: [Recipe]
+    getOneRecipe(_id: ID!): Recipe 
+    getRecipesByIds(_id: [ID]!): [Recipe] 
+    getPosts: [Post]
+    getPost(postId: ID!): Post
+  }
+   
+  input RecipeInput {
+    _id: String 
+    image: String
+    name: String
+    ingredients: [IngredientInput]
+    steps: [String]
+    totalTime: Int
+    serves: Int
+  }
+  input UpdateRecipeInput {
+    _id: String 
+    image: String
+    name: String
+    ingredients: [IngredientInput]
+    steps: [String]
+    totalTime: Int
+    serves: Int
+    createdAt: String
+  }
+  input IngredientInput {
+    name: String
+    qty: String
+  }
+   type Mutation {
+    login( email: String!, password: String!): Auth
+    createUser( username: String!, email: String!, password: String!): Auth
+    selectRecipe(_id: String! ): User
+    createRecipe( input: RecipeInput!): Recipe
+    removeRecipe(_id: ID!): User
+    updateRecipe( input: UpdateRecipeInput!): Recipe
+    deleteRecipe(_id: ID!): Recipe
+
+    createPost(body: String!): Post
+    deletePost(postId: ID!): String
+    createComment(postId: String, body: String): Post
+    deleteComment(postId: String, commentId: String): Post
+    likePost(postId: String!): Post
+   }
+`;
+
+module.exports = typeDefs;
