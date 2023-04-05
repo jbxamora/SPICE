2a3be8e686fac8462ef5539ab11ae32c8bf7b8cf
jbxamora <jbxamora@icloud.com>
2023-04-04 22:51:09 -0700

    created navbar component // added logo

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-[#020617] text-white fixed w-full top-0 left-0">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            <div className="text-2xl font-bold gradient-text">SPICE</div>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+            >
+              <i className={`fas fa-${isOpen ? "times" : "bars"}`} />
+            </button>
+          </div>
+          <div className={`hidden md:block ${isOpen ? "block" : "hidden"}`}>
+            <div className="ml-10 flex items-baseline space-x-4">
+              <a
+                href="#"
+                className="text-gray-300 hover:text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Home
+              </a>
+              <a
+                href="#"
+                className="text-gray-300 hover:text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Create
+              </a>
+
+              <a
+                href="#"
+                className="text-gray-300 hover:text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Sign Up
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={`${isOpen ? "block" : "hidden"} md:hidden`}>
+        <div className="px-2 pt-2 pb-3 space-y-1">
+          <a
+            href="#"
+            className="text-gray-300 hover:text-white hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:text-white hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Create
+          </a>
+
+          <a
+            href="#"
+            className="text-gray-300 hover:text-white hover:bg-gray-700 block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Sign Up
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
+  important: true,
 }
