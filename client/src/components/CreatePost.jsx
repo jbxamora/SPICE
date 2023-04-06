afbc6f50a9763169d44b3a6ef3c6835123da9f61
jbxamora <jbxamora@icloud.com>
2023-04-06 16:25:58 -0700

    updated createpost container to make it responsive

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -37,7 +37,7 @@ const CreatePost = () => {
   };
 
   return (
-    <div className="container w-45 mx-auto mt-10 px-4">
+    <div className="container w-full md:w-2/3 lg:w-1/2 mx-auto mt-10 px-4">
       {!preview && <h2 className="text-3xl font-bold mb-6">Create a Post</h2>}
       <form onSubmit={handleSubmit} className="w-full">
         <div className="mb-4">
