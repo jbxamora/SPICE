a08eac7c5d45e5706d6b1fe2190972f5ccb83e09
jbxamora <jbxamora@icloud.com>
2023-04-06 19:58:03 -0700

    created a 'feed' prototype and dummydata to display the cards with placeholder images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import React from "react";
-import Auth from "./components/Auth";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { dummyPosts } from "./constants/constants";
+
 import Navbar from "./components/Navbar";
 import CreatePost from "./components/CreatePost";
+import Auth from "./components/Auth";
+import PostsList from "./components/PostList";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<PostsList posts={dummyPosts} />} />
         <Route path="/signup" element={<Auth />} />
         <Route path="/create" element={<CreatePost />} />
       </Routes>
---
+++ client/src/constants/constants.jsx
@@ -0,0 +1,20 @@
+export const dummyPosts = [
+  {
+    id: 1,
+    title: "First Post",
+    imageUrl: "https://via.placeholder.com/150",
+    content: "<p>This is the first post content.</p>",
+  },
+  {
+    id: 2,
+    title: "Second Post",
+    imageUrl: "https://via.placeholder.com/150",
+    content: "<p>This is the second post content.</p>",
+  },
+  {
+    id: 3,
+    title: "Third Post",
+    imageUrl: "https://via.placeholder.com/150",
+    content: "<p>This is the third post content.</p>",
+  },
+];
